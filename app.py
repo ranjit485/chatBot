e36ee7bbe2ea05a20b179361c4e3a09d4376f76b
RanjitPatil <patilranjit485@gmail.com>
2024-03-07 19:03:32 -0800

    update 25

--- app.py
+++ app.py
@@ -115,4 +115,4 @@ def generate_ans():
 
 
 if __name__ == '__main__':
-    app.run(port=int(os.environ.get('PORT', 5000)), host='0.0.0.0')
+    app.run(port=int(os.environ.get('PORT', 9000)), host='0.0.0.0')
